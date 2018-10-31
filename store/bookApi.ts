e46a6cecfa42f4d8bec82eaba0e3ed38cb7eaca9
Mika Kuitunen <mika.kuitunen@solita.fi>
2018-10-31 16:39:52 +0200

    Add missing bookApi.ts

---
+++ store/bookApi.ts
@@ -0,0 +1,87 @@
+import { Action, ActionCreator, Dispatch, Reducer } from 'redux';
+import { ThunkAction, ThunkDispatch } from 'redux-thunk';
+
+// Not an authentication key, used for statistics.
+// TODO: restrict api key usage to Android/iOS apps only
+const apiKey: string = 'AIzaSyAiUlbaPvpP3QC-qnjl7_MXPQ7HY0RDgqg';
+
+export type BookApiState = Readonly<{
+  isBusy: boolean,
+  error: Error | undefined
+}>;
+
+const defaultState: BookApiState = {
+  isBusy: false,
+  error: undefined
+};
+
+// TS 'typeof' returns the exact value as type for implicitly typed 'const'
+const FETCH_BOOK_START = 'refgen-app/bookApi/FETCH_BOOK_START';
+const FETCH_BOOK_SUCCESS = 'refgen-app/bookApi/FETCH_BOOK_SUCCESS';
+const FETCH_BOOK_ERROR = 'refgen-app/bookApi/FETCH_BOOK_ERROR';
+
+interface FetchBookStartAction extends Action {
+  type: typeof FETCH_BOOK_START;
+};
+
+interface FetchBookSuccessAction extends Action {
+  type: typeof FETCH_BOOK_SUCCESS;
+  payload: BookResponse;
+};
+
+interface FetchBookErrorAction extends Action {
+  type: typeof FETCH_BOOK_ERROR;
+  payload: Error;
+};
+
+type KnownAction = FetchBookStartAction | FetchBookSuccessAction | FetchBookErrorAction;
+
+export default function reducer (state: BookApiState = defaultState, action: KnownAction): BookApiState {
+  switch (action.type) {
+    case FETCH_BOOK_START: {
+      return {
+        ...state,
+        isBusy: true
+      };
+    }
+    case FETCH_BOOK_SUCCESS: {
+      return {
+        ...state,
+        isBusy: false // TODO: do something with the data
+      };
+    }
+    case FETCH_BOOK_ERROR: {
+      return {
+        ...state,
+        isBusy: false,
+        error: action.payload
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+}
+
+export function fetchBook(barCode: string): ThunkAction<void, undefined, undefined, KnownAction> {
+  return (dispatch: ThunkDispatch<undefined, undefined, KnownAction>) => {
+    dispatch({ type: FETCH_BOOK_START });
+
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=${barCode}&key=${apiKey}`)
+      .then((response: Response) => {
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+
+        return response.json();
+      })
+        .then((responseData: BookResponse) => {
+          // TODO: Process the data to bare minimum
+          dispatch({ type: FETCH_BOOK_SUCCESS, payload: responseData });
+        })
+      .catch((error: Error) => {
+        console.log(error);
+        dispatch({ type: FETCH_BOOK_ERROR, payload: error });
+      });
+  }
+}
